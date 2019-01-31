cc308241deabbc811067a4abf59eaef6224966dc
starskynder <24276076+starskynder@users.noreply.github.com>
2019-01-31 11:58:41 +0100

    Let Navigation menu disappear when a navigation link is clicked

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ function showMenu() {
   navigationList.classList.toggle("navigation__list--active");
   menuButton.classList.toggle("navigation__btn--active");
 }
+
+const navigationLink = document.querySelectorAll(".navigation__link");
+
+navigationLink.forEach(v => {
+  v.addEventListener("click", closeMenu);
+});
+
+function closeMenu() {
+  navigationList.classList.remove("navigation__list--active");
+  menuButton.classList.remove("navigation__btn--active");
+}
